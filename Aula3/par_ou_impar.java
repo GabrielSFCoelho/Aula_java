726aaeae10bff1e8d8a9f6f8224cb89496b9bded
GabrielSFCoelho <88520568+GabrielSFCoelho@users.noreply.github.com>
2022-03-23 22:08:17 -0300

    Add files via upload

---
+++ Aula3/par_ou_impar.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public static String verificarParImpar (int n){
+    if(i % 2 == 0){
+       return = "É par !";}
+        
+        else{
+             return "É impar!";}
+
+   
+
+}
+public class par_ou_impar {
+    public static void main(String[] args) {
+    Scanner teclado = new Scanner (system.in);
+    int n;
+    do{
+        int n = teclado.nextInt();
+        if ( n>= 0){
+            system.out.println(verificarParImpar)
+        }
+    while(n >=0);
+    teclado.close();
+    
+    }
+       
+    
+    }
+}
+
