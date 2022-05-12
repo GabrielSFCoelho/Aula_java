c8af7d6cd456b88ecb74637ab2b41f2daa24a12a
GabrielSFCoelho <88520568+GabrielSFCoelho@users.noreply.github.com>
2022-05-11 22:08:46 -0300

    Add files via upload

---
+++ Aula5-Polimorfismo/logica/ProgamPrincipal.java
@@ -0,0 +1,12 @@
+package logica;
+
+import grafica.frmCalculadoraGeomtrica;
+
+public class ProgamPrincipal {
+    public static void main(String[] args) {
+        frmCalculadoraGeomtrica frm =  new frmCalculadoraGeomtrica();
+        
+        frm.setVisible(true);
+
+    }
+}
